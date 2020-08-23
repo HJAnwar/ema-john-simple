ef687ac25d6354d25e7a6686a36df329348bc4f8
Anwar4123 <anwarhossenmehidi@gmail.com>
2020-08-23 12:00:41 -0700

    added

---
+++ src/componen/heddar/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from'../../images/logo.png';
+import './Header.css';
+
+const Heddar = () => {
+    return (
+        <div className="header">
+           <img src={logo} alt=""/> 
+        </div>
+    );
+};
+
+export default Heddar;
